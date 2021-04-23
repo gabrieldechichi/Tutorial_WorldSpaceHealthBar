39f45a0dfdb376a239a3c83278a1dcf013962635
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-04-23 09:28:38 -0600

    Health bar setup

---
+++ HealthBar/Assets/_Game/Scripts/Runtime/UI/UIWorldBillboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class UIWorldBillboard : MonoBehaviour
+{
+    private Camera Cam => cam == null ? (cam = Camera.main) : cam;
+    private Camera cam;
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + Cam.transform.rotation * Vector3.forward, Cam.transform.rotation * Vector3.up);
+    }
+}
