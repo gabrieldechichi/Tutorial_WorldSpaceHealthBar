3cfdc02c89d8d6b17b6ca11a0176e46c392ee227
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-05-03 19:06:14 -0600

    Add health bar speed

--- HealthBar/Assets/_Game/Scripts/Runtime/UI/HealthBarUI.cs
+++ HealthBar/Assets/_Game/Scripts/Runtime/UI/HealthBarUI.cs
@@ -9,13 +9,17 @@ public class HealthBarUI : MonoBehaviour
     [SerializeField]
     private DamageableBehaviour damageable;
 
+    [SerializeField]
+    [Min(0.1f)]
+    private float speed = 2;
+
     private void LateUpdate()
     {
         if (damageable != null)
         {
             //1 when CurrentHealth = MaxHealth, 0 when CurrentHealth = 0
             var healthPercent = (float) damageable.CurrentHealth / damageable.MaxHealth;
-            healthBarFill.fillAmount = healthPercent;
+            healthBarFill.fillAmount = Mathf.Lerp(healthBarFill.fillAmount, healthPercent, Time.deltaTime*speed);
         }
     }
 }
